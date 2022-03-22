9909d44b216f3da08ce531bc0102f9160681b87f
Jari Sääksi <jari.saaksi@gmail.com>
2022-03-22 13:17:59 +0200

    Salasanajuttuja2 23.3

--- app.js
+++ app.js
@@ -1,47 +1,36 @@
-const express=require('express')
-const dotenv=require('dotenv')
+const express = require('express');
+const dotenv=require('dotenv');
 const cors=require('cors');
 const bookRouter=require('./routes/book.js');
 const exampleRouter=require('./routes/example');
 const basicAuth = require('express-basic-auth');
 const loginRouter=require('./routes/login');
 
-
 const app = express();
 app.use(cors());
 dotenv.config();
 app.use(express.urlencoded({extended:false}));
-
-// app.use(basicAuth({users: { 'admin': '1234' }}))
-app.use(basicAuth( { authorizer: myAuthorizer, authorizeAsync:true, } ))
-
-app.use('/book',bookRouter);
 app.use('/example',exampleRouter);
 app.use('/login',loginRouter);
+app.use(basicAuth( { authorizer: myAuthorizer, authorizeAsync:true, } ));
+app.use('/book',bookRouter);
+
 
 function myAuthorizer(username, password, cb){
-    if(username===process.env.authUser && password ===process.env.authPassword){
-        return cb(null, true);
-    }
-    else{
-        return cb(null, false);
-    }
+  if(username===process.env.authUser && password ===process.env.authPass){
+      return cb(null, true);
+  }
+  else{
+      return cb(null, false);
+  }
 }
 
-const dotenv = require('dotenv');
-dotenv.config();
 var port=process.env.PORT;
-
-
 app.listen(port, 
   function(){
     console.log('App listening on port '+port)
   },
 );
-
-
-
-
 
 app.get('/',function(request,response){
     console.log("polkua /koe  ");
